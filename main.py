9dda65c758a9a6f74b728496ad7a55fc98dc4bec
PramodThapa <thapapramod821@gmail.com>
2022-10-10 18:37:15 +0545

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+# To Generate Brand Name using Users input
+print("Welcome To Brand Name Generator:")
+city = input("Enter Your Born City\n")
+pet = input("Enter Your Favourite Pet Name\n")
+print("Your Brand Name will be:" + city + " " + "" + pet)
+
